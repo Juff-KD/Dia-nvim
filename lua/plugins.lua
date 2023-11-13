a20feb6c8196ff98685c4c0c44aeb74b85010209
Juff-KD <juffcade@gmail.com>
2023-11-13 14:28:14 +0630

    codeium modified

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -102,7 +102,7 @@ local plugins ={
 },
 {
   'Exafunction/codeium.vim',
-      -- event = 'BufEnter',
+      event = 'BufEnter',
       config = function ()
     -- Change '<C-g>' here to any keycode you like.
     vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true })
