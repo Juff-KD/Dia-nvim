973a0f13c3bdf6f4a37cc7f17093043eb0c90879
Juff-KD <96113014+Juff-KD@users.noreply.github.com>
2024-01-31 22:19:36 +0630

    Jan-1 update

--- lua/config/toggle_term.lua
+++ lua/config/toggle_term.lua
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 local Terminal = require("toggleterm.terminal").Terminal
 local lazygit = Terminal:new({
   cmd = "lazygit",
@@ -11,17 +10,3 @@ function _Lazygit_toggle()
 end
 
 vim.api.nvim_set_keymap('n', '<space>g', "<cmd>lua _lazygit_toggle()<cr>", { noremap = true, silent = true })
-=======
-local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new({
-  cmd = "lazygit",
-  close_on_exit = true,
-  direction = "float"
-})
-
-function _lazygit_toggle()
-  lazygit:toggle()
-end
-
-vim.api.nvim_set_keymap('n', '<space>g', "<cmd>lua _lazygit_toggle()<cr>", { noremap = true, silent = true })
->>>>>>> 71418ecfb8b1f19643bd18cbb754497c7171cfdb
