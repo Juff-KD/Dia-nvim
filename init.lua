2f661879c2b0eb7c1a525aa32b40bf0f91d8eb83
Juff-KD <96113014+Juff-KD@users.noreply.github.com>
2024-01-31 22:09:07 +0630

    Jan-1 update

--- init.lua
+++ init.lua
@@ -1,4 +1,11 @@
+<<<<<<< HEAD
 require ('options')
 require ('plugins')
 require ('keymaps')
 require ('themes')
+=======
+require ('options')
+require ('plugins')
+require ('keymaps')
+require ('themes')
+>>>>>>> 71418ecfb8b1f19643bd18cbb754497c7171cfdb
