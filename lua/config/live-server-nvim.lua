8306a9c968b926f495a003f3be74e4cb5992d474
Juff-KD <juffcade@gmail.com>
2024-05-09 14:08:16 +0630

    I did this
    I deleted

--- lua/config/live-server-nvim.lua
+++
@@ -1,9 +0,0 @@
-require('live-server-nvim').setup({
-    custom = {
-        "--port=8080",
-        "--quiet",
-        "--no-css-inject",
-    },
- serverpath = vim.fn.stdpath("data") .. "/live-server/",
- open = "folder", --folder|cwd
-})
